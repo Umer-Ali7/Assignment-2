1b32454ffdbb061de9e347f902e75b061fc4c2a4
Umer Ali <160734211+Umer-Ali7@users.noreply.github.com>
2024-09-21 18:32:15 +0500

    Add files via upload

---
+++ Header.tsx
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <div>
+      <hr />
+      <h1>Header</h1>
+      <p>This is a Header Page</p>
+    </div>
+  )
+};
+
+export default Header;
---
+++ page.tsx
@@ -0,0 +1,15 @@
+import About from "./components/About";
+import Header from "./components/Header";
+
+function Home() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <Header />
+      <hr />
+      <About />
+    </div>
+  )
+};
+
+export default Home;
